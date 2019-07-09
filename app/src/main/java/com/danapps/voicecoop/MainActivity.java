1ee238f579becc258ce2574ddae7f94fd8190748
DanielPri <privorotskyd@gmail.com>
2019-07-09 19:27:46 -0400

    Added shake detection using open source api
    Using sensey by nisrulz on github

--- app/src/main/java/com/danapps/voicecoop/MainActivity.java
+++ app/src/main/java/com/danapps/voicecoop/MainActivity.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.nisrulz.sensey.Sensey;
+import com.github.nisrulz.sensey.ShakeDetector;
+
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -17,14 +20,30 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView voiceOutput;
 
+    ShakeDetector.ShakeListener shakeListener = new ShakeDetector.ShakeListener() {
+        @Override public void onShakeDetected() {
+            //shake started, do something
+            voiceOutput.setText("SHAKING");
+            getSpeechInput(null);
+        }
+
+        @Override public void onShakeStopped() {
+           //Done shaking
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         voiceOutput = findViewById(R.id.voiceOutput);
+        Sensey.getInstance().init(this);
+        Sensey.getInstance().startShakeDetection(shakeListener);
     }
 
     public void getSpeechInput(View view){
+        Sensey.getInstance().stopShakeDetection(shakeListener);
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
@@ -46,8 +65,16 @@ public class MainActivity extends AppCompatActivity {
                 if(resultCode == RESULT_OK && data != null){
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     voiceOutput.setText(result.get(0));
+                    Sensey.getInstance().startShakeDetection(shakeListener);
                 }
                 break;
         }
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Sensey.getInstance().stop();
+    }
+
 }
